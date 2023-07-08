601db9317f44af3d6b9021f91798d55fbff135fc
XRIN <xrin6666@gmail.com>
2023-07-08 23:54:41 +0800

    添加权限注解

--- src/main/java/com/xjh/security_demo/controller/SecurityController.java
+++ src/main/java/com/xjh/security_demo/controller/SecurityController.java
@@ -1,5 +1,6 @@
 package com.xjh.security_demo.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SecurityController {
 
     @GetMapping("hello")
+    @PreAuthorize("hasAuthority('test:hello')")
     public String hello(){
         return "hello xjh";
     }
